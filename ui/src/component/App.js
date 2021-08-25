91fbaccd07b7df4a38f8b6b8ca8f94f99760196b
Pierre Verbe <pierre.verbe@free.fr>
2021-08-25 14:28:10 +0200

    Added AppBar

--- ui/src/component/App.js
+++ ui/src/component/App.js
@@ -1,80 +1,69 @@
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
+import React from "react"
+import { BrowserRouter, Link, Route, Switch } from "react-router-dom"
 
-import UIStudent from "./student/UIStudent"
 import Home from "./home/Home"
+import UIStudent from "./student/UIStudent"
+
 import './App.css'
-
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 
 const App = () => {
-  const classes = useStyles();
+  const routes = ["/", "/strategy", "/monitoring"]
 
   return (
-  <div>
+    <div className="App">
+      <BrowserRouter>
+        <Route
+          path="/"
+          render={(history) => (
+            <AppBar>
+              <Tabs
+                value={
+                  history.location.pathname !== ""
+                    ? history.location.pathname
+                    : false
+                }
+              >
+                <Tab
+                  value={routes[0]}
+                  label="Home"
+                  component={Link}
+                  to={routes[0]}
+                />
+                <Tab
+                  value={routes[1]}
+                  label="Strategy"
+                  component={Link}
+                  to={routes[1]}
+                />
+                <Tab
+                  value={routes[2]}
+                  label="Monitoring"
+                  component={Link}
+                  to={routes[2]}
+                />
+              </Tabs>
+            </AppBar>
+          )}
+        />
 
-
-    <AppBar position="static">
-      <Toolbar variant="dense">
-        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          Photos
-        </Typography>
-      </Toolbar>
-    </AppBar>
-
-
-
-    <Router>
-      <div id="header">
-        <nav>
-          <ul>
-            <Link to="/">
-              <p><span className="nav-btn">Home</span></p>
-            </Link>
-            <Link to="/strategy">
-              <p><span className="nav-btn">Strategy</span></p>
-            </Link>
-            <Link to="/monitoring">
-              <p><span className="nav-btn">Monitoring</span></p>
-            </Link>
-          </ul>
-        </nav>
-      </div>
-      <div>
         <Switch>
           <Route path="/strategy">
-            <p>Strategy part</p>
+            <h1 style={{ marginTop: "10vh" }}>Strategy part</h1>
             <UIStudent/>
           </Route>
-          <Route path="/monitoring">
-            <p>Monitoring part</p>
+          <Route path="/monitoring" >
+            <h1 style={{ marginTop: "10vh" }}>Monitoring part</h1>
           </Route>
-          <Route path="/">
+          <Route path="/" >
+            <h1 style={{ marginTop: "10vh" }}>Home part</h1>
             <Home/>
           </Route>
         </Switch>
-      </div>
-    </Router>
-  </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
