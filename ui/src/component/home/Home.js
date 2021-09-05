a099abb54b9b39fd4fba9f5a3e03a0a4cb962fae
Pierre Verbe <pierre.verbe@free.fr>
2021-09-05 23:40:25 +0200

    Added text home page

--- ui/src/component/home/Home.js
+++ ui/src/component/home/Home.js
@@ -7,17 +7,22 @@ const Home = () => {
     return (
         <div>
             <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
-            
+            <div id="textHome">
+                <h1>Vieux Roboticiens Amis de Cachan</h1>
+                <h2>Participations :</h2>
+                <p>Édition 2018 Coupe de France de robotique (CRAC) : 4 ème</p>
+                <p>Édition 2019</p>
+                <ul>
+                    <li>Coupe de France de robotique : 34 ème</li>
+                    <li>Coupe de D'île de france : ? ème</li>
+                </ul>
+                <p>Édition 2020 Coupe de France de robotique : 9 ème</p>
+                <p>Édition 2021</p>
+                <ul>
+                    <li>Coupe de France de robotique : 9 ème</li>
+                    <li>Coupe de D'île de france : ? ème</li>
+                </ul>
+            </div>
         </div>
     )
 }
