7b957107d3e19ad9b7ce25f0a6c688945effefd1
Pierre Verbe <pierre.verbe@free.fr>
2021-10-23 23:28:08 +0200

    remove API

--- API/src/main/java/com/vrac/restservice/entity/MongoCollection.java
+++
@@ -1,6 +0,0 @@
-package com.vrac.restservice.entity;
-
-public interface MongoCollection {
-    public static final String SEQUENCE = "Sequence";
-    public static final String STRATEGY = "Strategy";
-}
