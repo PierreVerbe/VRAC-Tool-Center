d55a0cb7bb0c388dcefcc1cb98b14d47aee40715
Pierre Verbe <pierre.verbe@free.fr>
2021-10-25 16:39:56 +0200

    Improved toolbar with icon night mode and cleaned code

--- ui/src/component/general/Header.js
+++ ui/src/component/general/Header.js
@@ -2,20 +2,17 @@ import React from "react"
 import { connect } from "react-redux"
 import { BrowserRouter, Link, Route, Switch as SwitchRouter } from "react-router-dom"
 
+import { Switch, AppBar, Tabs, Tab } from '@material-ui/core'
+import Toolbar from '@material-ui/core/Toolbar'
+import MenuIcon from '@material-ui/icons/Menu'
+import Brightness4Icon from '@material-ui/icons/Brightness4'
+
 import Home from "./../home/Home"
 import UIStudent from "./../student/UIStudent"
 import Monitoring from "./../monitoring/Monitoring"
-
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-import Toolbar from '@material-ui/core/Toolbar';
-
-import MoreIcon from '@material-ui/icons/MoreVert';
-import './Header.css'
-import { Switch, AppBar, Tabs, Tab } from '@material-ui/core'
-
 import { setDarkModeActionCreator } from "./../../action/generalAction"
+
+import './Header.css'
 
 const Header = ({darkMode, setDarkMode}) => {
     const routes = ["/", "/strategy", "/monitoring"]
@@ -26,40 +23,35 @@ const Header = ({darkMode, setDarkMode}) => {
                 <Route
                     path="/"
                     render={(history) => (
-                    <AppBar color="primary" position="static">
-                        <Toolbar  variant="dense">
-                            <IconButton edge="start" color="inherit" aria-label="menu">
+                        <AppBar color="primary" position="static">
+                            <Toolbar  variant="dense">
                                 <MenuIcon />
-                            </IconButton>
-                            <Tabs value={history.location.pathname !== "" ? history.location.pathname : false}>
-                        <Tab
-                            value={routes[0]}
-                            label="Home"
-                            component={Link}
-                            to={routes[0]}
-                        />
-                        <Tab
-                            value={routes[1]}
-                            label="Strategy"
-                            component={Link}
-                            to={routes[1]}
-                        />
-                        <Tab
-                            value={routes[2]}
-                            label="Monitoring"
-                            component={Link}
-                            to={routes[2]}
-                        />  
-                        </Tabs>
-                        
-                            <IconButton aria-label="display more actions" edge="end" color="inherit">
-                                <MoreIcon />
-                            </IconButton>
-                            <Switch className="switchDarkMode" color="secondary" checked={darkMode} onChange={() => setDarkMode(!darkMode)}></Switch>
-                            
-                        </Toolbar>
-                        
-                    </AppBar>
+
+                                <Tabs value={history.location.pathname !== "" ? history.location.pathname : false}>
+                                    <Tab
+                                        value={routes[0]}
+                                        label="Home"
+                                        component={Link}
+                                        to={routes[0]}
+                                    />
+                                    <Tab
+                                        value={routes[1]}
+                                        label="Strategy"
+                                        component={Link}
+                                        to={routes[1]}
+                                    />
+                                    <Tab
+                                        value={routes[2]}
+                                        label="Monitoring"
+                                        component={Link}
+                                        to={routes[2]}
+                                    />  
+                                </Tabs>
+
+                                <Brightness4Icon />
+                                <Switch className="switchDarkMode" color="secondary" checked={darkMode} onChange={() => setDarkMode(!darkMode)}></Switch>
+                            </Toolbar>
+                        </AppBar>
                     )}
                 />
 
@@ -68,12 +60,14 @@ const Header = ({darkMode, setDarkMode}) => {
                         <h1 style={{ marginTop: "10vh" }}>Strategy part</h1>
                         <UIStudent/>
                     </Route>
+
                     <Route path="/monitoring" >
                         <h1 style={{ marginTop: "10vh" }}>Monitoring part</h1>
                         <Monitoring/>
                     </Route>
-                        <Route path="/" >
-                    <Home/>
+                    
+                    <Route path="/" >
+                        <Home/>
                     </Route>
                 </SwitchRouter>
             </BrowserRouter>
