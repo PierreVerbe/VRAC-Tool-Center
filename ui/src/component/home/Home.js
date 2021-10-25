bc06856102f85eb3cbe1bccad5412e00670f7293
Pierre Verbe <pierre.verbe@free.fr>
2021-10-25 22:19:04 +0200

    Added css on home paper

--- ui/src/component/home/Home.js
+++ ui/src/component/home/Home.js
@@ -1,14 +1,30 @@
 import React from "react"
 
-import Paper from '@material-ui/core/Paper';
+import Paper from '@material-ui/core/Paper'
+import { makeStyles } from '@material-ui/core/styles'
 
 import logo from "./../../resources/image/logo_VRAC.png"
 
 import "./Home.css"
 
+const useStyles = makeStyles({
+    root: {
+      padding: '20px',
+      height: '80vh',
+    },
+    paper: {
+      width: '100%',
+      height: '100%',
+    },
+  })
+
 const Home = () => {
+    const classes = useStyles()
+
     return (
-        <Paper elevation={3} >
+        <div className={classes.root}>
+
+        <Paper elevation={3} className={classes.paper}>
             <img id="logoVracHome" alt="Vrac Logo" src={logo}/>
             <div id="textHome">
                 <h1>Vieux Roboticiens Amis de Cachan</h1>
@@ -27,6 +43,8 @@ const Home = () => {
                 </ul>
             </div>
         </Paper>
+       
+        </div>
     )
 }
 
