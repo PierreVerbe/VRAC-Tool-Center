7af08747a6b19a1a1cb261844cd03d762cd7ab37
Pierre Verbe <pierre.verbe@free.fr>
2021-10-25 22:57:27 +0200

    Base load/delete for monitoring

---
+++ ui/src/component/monitoring/Monitoring.js
@@ -0,0 +1,225 @@
+import React from "react"
+import { connect } from "react-redux"
+import PropTypes from "prop-types"
+import clsx from "clsx"
+
+import StepConnector from "@material-ui/core/StepConnector"
+import { makeStyles, withStyles } from "@material-ui/core/styles"
+import Paper from '@material-ui/core/Paper'
+import Stepper from "@material-ui/core/Stepper"
+import Step from "@material-ui/core/Step"
+import StepLabel from "@material-ui/core/StepLabel"
+import Button from "@material-ui/core/Button"
+import Typography from "@material-ui/core/Typography"
+
+import DesktopWindowsIcon from '@material-ui/icons/DesktopWindows';
+import EqualizerIcon from '@material-ui/icons/Equalizer';
+import GetAppIcon from '@material-ui/icons/GetApp';
+
+import LoadDelete from "./step/load/LoadDelete"
+import Simulation from "./step/Simulation"
+import Statistic from "./step/Statistic"
+import { setMonitoringStepperActionCreator } from "./../../action/monitoringAction"
+
+import "./Monitoring.css"
+
+const ColorlibConnector = withStyles({
+  alternativeLabel: {
+    top: 22
+  },
+  active: {
+    "& $line": {
+      backgroundImage:
+        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)"
+    }
+  },
+  completed: {
+    "& $line": {
+      backgroundImage:
+        "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)"
+    }
+  },
+  line: {
+    height: 3,
+    border: 0,
+    backgroundColor: "#eaeaf0",
+    borderRadius: 1
+  }
+})(StepConnector)
+
+const useColorlibStepIconStyles = makeStyles({
+  root: {
+    backgroundColor: "#ccc",
+    zIndex: 1,
+    color: "#fff",
+    width: 50,
+    height: 50,
+    display: "flex",
+    borderRadius: "50%",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  active: {
+    backgroundImage:
+      "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
+    boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)"
+  },
+  completed: {
+    backgroundImage:
+      "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)"
+  }
+})
+
+function ColorlibStepIcon(props) {
+  const classes = useColorlibStepIconStyles()
+  const { active, completed } = props
+
+  const icons = {
+    1: <GetAppIcon />,
+    2: <DesktopWindowsIcon />,
+    3: <EqualizerIcon />
+  }
+
+  return (
+    <div
+      className={clsx(classes.root, {
+        [classes.active]: active,
+        [classes.completed]: completed
+      })}
+    >
+      {icons[String(props.icon)]}
+    </div>
+  )
+}
+
+ColorlibStepIcon.propTypes = {
+  /**
+   * Whether this step is active.
+   */
+  active: PropTypes.bool,
+  /**
+   * Mark the step as completed. Is passed to child components.
+   */
+  completed: PropTypes.bool,
+  /**
+   * The label displayed in the step icon.
+   */
+  icon: PropTypes.node
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: '20px',
+      height: '80vh',
+  },
+  paper: {
+    width: '100%',
+    height: '100%',
+  },
+  button: {
+    marginRight: theme.spacing(1)
+  },
+  instructions: {
+    marginTop: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  }
+}))
+
+function getSteps() {
+  return ["Load/Delete", "Simulation", "Statistic"]
+}
+
+function getStepContent(step) {
+  switch (step) {
+    case 0:
+      return <LoadDelete/>
+    case 1:
+      return <Simulation/>
+    case 2:
+      return <Statistic/>
+    default:
+      return "Unknown step"
+  }
+}
+
+const Monitoring = ({monitoringStepper, setMonitoringStepper}) => {
+
+  const classes = useStyles()
+  const steps = getSteps()
+
+  const handleNext = () => {
+    setMonitoringStepper(monitoringStepper + 1)
+  }
+
+  const handleBack = () => {
+    setMonitoringStepper(monitoringStepper - 1)
+  }
+
+  const handleReset = () => {
+    setMonitoringStepper(0)
+  }
+
+    return (
+      <div className={classes.root}>
+      <Paper elevation={3} className={classes.paper}>
+        
+        <Stepper
+          alternativeLabel
+          activeStep={monitoringStepper}
+          connector={<ColorlibConnector />}
+        >
+          {steps.map((label) => (
+            <Step key={label}>
+              <StepLabel StepIconComponent={ColorlibStepIcon}>{label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+        <div>
+          {monitoringStepper === steps.length ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed - you&aposre finished
+              </Typography>
+              <Button onClick={handleReset} className={classes.button}>
+                Reset
+              </Button>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>
+                {getStepContent(monitoringStepper)}
+              </Typography>
+              <div>
+                <Button
+                  disabled={monitoringStepper === 0}
+                  onClick={handleBack}
+                  className={classes.button}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={handleNext}
+                  className={classes.button}
+                >
+                  {monitoringStepper === steps.length - 1 ? "Finish" : "Next"}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+        </Paper>
+      </div>
+  )
+}
+
+const mapStateToProps = state => ({
+  monitoringStepper: state.monitoringStepper
+})
+
+const mapDispatchToProps = dispatch => ({
+  setMonitoringStepper: monitoringStepper => dispatch(setMonitoringStepperActionCreator(monitoringStepper))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Monitoring)
---
+++ ui/src/component/monitoring/step/load/LoadDelete.js
@@ -0,0 +1,67 @@
+import React from "react"
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+
+import TableMonitoring from "./TableMonitoring";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        flexGrow: 1,
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
+    },
+    iconButton: {
+        padding: 10,
+      },
+    input: {
+    marginLeft: theme.spacing(1),
+    flex: 1,
+    },
+    lol: {
+        
+    }
+  }));
+
+const LoadDelete = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+            <Grid item xs={6}>
+                <Paper component="form">
+                    <IconButton className={classes.iconButton} aria-label="menu">
+                        <MenuIcon />
+                    </IconButton>
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Search VRAC Monitoring"
+                        inputProps={{ 'aria-label': 'search vrac monitoring' }}
+                    />
+                    <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                        <SearchIcon />
+                    </IconButton>
+                    <TableMonitoring/>
+                </Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper}>xs=6</Paper>
+                
+            </Grid>
+            </Grid>
+        </div>
+    )
+}
+
+export default LoadDelete
