b7ba415b2dada986eb2ef01aa80ab0974fb48aef
Pierre Verbe <pierre.verbe@free.fr>
2021-10-27 15:06:09 +0200

    Added clear button

--- ui/src/component/monitoring/step/load/SelectedMonitoring.js
+++ ui/src/component/monitoring/step/load/SelectedMonitoring.js
@@ -3,11 +3,20 @@ import ReactJson from 'react-json-view'
 import { connect } from "react-redux"
 import PropTypes from 'prop-types'
 
-import {DropzoneArea} from 'material-ui-dropzone'
+import Button from "@material-ui/core/Button"
+import { makeStyles } from '@material-ui/core/styles'
+import { DropzoneArea } from 'material-ui-dropzone'
 
 import { setIdRowMonitoringTableActionCreator, setMonitoringActionCreator } from "./../../../../action/monitoringAction"
 
-
+const useStyles = makeStyles({
+    root: {
+      width: '100%',
+    },
+    container: {
+      maxHeight: 500,
+    },
+  })
 
 const DropzoneOrReactJson = (props) => {
     const { isSelected, monitoring, setIdRowMonitoringTable, setMonitoring } = props
@@ -43,7 +52,6 @@ const DropzoneOrReactJson = (props) => {
     else {
        return (
            <div>
-            <p>UN truc</p>
             <ReactJson src={monitoring} />
            </div>
        )
@@ -60,10 +68,29 @@ DropzoneOrReactJson.propTypes = {
 const SelectedMonitoring = ({idRowMonitoringTable, monitoring, setIdRowMonitoringTable, setMonitoring}) => {
 console.log(idRowMonitoringTable)
 
+//const classes = useStyles()
+
+const handleClear = () => {
+    if (monitoring !== {}) {
+        setMonitoring({})
+    }
+    if (idRowMonitoringTable !== null) {
+        setIdRowMonitoringTable(null)
+    }
+}
+
     return (
         <div>
             <p>Simulation section</p>
             <DropzoneOrReactJson isSelected={idRowMonitoringTable !== null} monitoring={monitoring} setIdRowMonitoringTable={setIdRowMonitoringTable} setMonitoring={setMonitoring} />
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={handleClear}
+                //className={classes.button}
+            >
+            Clear
+            </Button>
         </div>
     )
 }
