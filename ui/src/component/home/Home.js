a0f7e2dc22e6191be0c44d1a0fc27d69f4c24f9a
Pierre Verbe <pierre.verbe@free.fr>
2021-11-24 00:02:49 +0100

    Cleaned css

--- ui/src/component/home/Home.js
+++ ui/src/component/home/Home.js
@@ -5,26 +5,12 @@ import Paper from '@material-ui/core/Paper'
 import logo from "./../../resources/image/logo_VRAC.png"
 import { ELEVATION_PAGE_CONTENT } from "../GeneralStyle"
 
-import { makeStyles } from '@material-ui/core/styles'
 import "./Home.css"
 
-const useStyles = makeStyles({
-    root: {
-        padding: '20px'
-    },
-    paper: {
-        display: 'inline-block',
-        width: '100%',
-        height: 'calc(100vh - 88px)'
-    },
-})
-
 const Home = () => {
-    const classes = useStyles()
-
     return (
-        <div id="rootHome"className={classes.root}>
-            <Paper className={classes.paper} elevation={ELEVATION_PAGE_CONTENT}>
+        <div id="rootHome">
+            <Paper id="paperHome" elevation={ELEVATION_PAGE_CONTENT}>
                 <img id="logoVracHome" alt="Vrac Logo" src={logo} />
             </Paper>
         </div>
