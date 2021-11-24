3429c9d59ded2c27f44c4830d2b0272afe0d98f9
Pierre Verbe <pierre.verbe@free.fr>
2021-11-24 15:22:58 +0100

    Splitted code in style.js

--- ui/src/component/strategy/Strategy.js
+++ ui/src/component/strategy/Strategy.js
@@ -3,8 +3,6 @@ import { connect } from "react-redux"
 import PropTypes from "prop-types"
 import clsx from "clsx"
 
-import StepConnector from "@material-ui/core/StepConnector"
-import { makeStyles, withStyles } from "@material-ui/core/styles"
 import Paper from '@material-ui/core/Paper'
 import Stepper from "@material-ui/core/Stepper"
 import Step from "@material-ui/core/Step"
@@ -19,78 +17,9 @@ import GetAppIcon from '@material-ui/icons/GetApp'
 import CreateStrategy from "./step/create/CreateStrategy"
 import LoadDeleteStrategy from "./step/loadDelete/LoadDeleteStrategy"
 import StatisticStrategy from "./step/statistic/StatisticStrategy"
-
-
 import { setStrategyStepperActionCreator } from "./../../action/strategyAction"
-
-import { ELEVATION_PAGE_CONTENT } from "./../Style"
-
+import { ELEVATION_PAGE_CONTENT, ColorlibConnector, useColorlibStepIconStyles, useStyles } from "./../Style"
 import "./../Style.css"
-
-const ColorlibConnector = withStyles({
-    alternativeLabel: {
-        top: 22
-    },
-    active: {
-        "& $line": {
-            backgroundImage:
-                "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)"
-        }
-    },
-    completed: {
-        "& $line": {
-            backgroundImage:
-                "linear-gradient( 95deg,rgb(242,113,33) 0%,rgb(233,64,87) 50%,rgb(138,35,135) 100%)"
-        }
-    },
-    line: {
-        height: 3,
-        border: 0,
-        backgroundColor: "#eaeaf0",
-        borderRadius: 1
-    }
-})(StepConnector)
-
-const useColorlibStepIconStyles = makeStyles({
-    root: {
-        backgroundColor: "#ccc",
-        zIndex: 1,
-        color: "#fff",
-        width: 50,
-        height: 50,
-        display: "flex",
-        borderRadius: "50%",
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    active: {
-        backgroundImage:
-            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)",
-        boxShadow: "0 4px 10px 0 rgba(0,0,0,.25)"
-    },
-    completed: {
-        backgroundImage:
-            "linear-gradient( 136deg, rgb(242,113,33) 0%, rgb(233,64,87) 50%, rgb(138,35,135) 100%)"
-    }
-})
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        padding: '20px',
-        height: '80vh',
-    },
-    paper: {
-        width: '100%',
-        height: '100%',
-    },
-    button: {
-        marginRight: theme.spacing(1)
-    },
-    instructions: {
-        marginTop: theme.spacing(1),
-        marginBottom: theme.spacing(1)
-    }
-}))
 
 ColorlibStepIcon.propTypes = {
     active: PropTypes.bool,
