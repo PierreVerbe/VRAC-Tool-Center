c1e437fb4240edea848591efa58d523ee37d486b
Pierre Verbe <pierre.verbe@socgen.com>
2021-12-01 21:10:25 +0100

    add drag and drop react flow

--- ui/src/component/strategy/step/create/CreateStrategy.js
+++ ui/src/component/strategy/step/create/CreateStrategy.js
@@ -1,11 +1,43 @@
-import React from "react"
+import { makeStyles } from '@material-ui/core/styles'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+
+import DnDFlow from "./right/GraphCreator"
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: '2px 4px',
+        display: 'flex',
+        alignItems: 'center',
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        color: theme.palette.text.secondary,
+    }
+}))
 
 const CreateStrategy = () => {
+    const classes = useStyles()
+
     return (
-        <div>
-            <p>CreateStrategy section</p>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={6}>
+                    <Paper className={classes.paper} component="form">
+                        
+                    </Paper>
+                </Grid>
+
+                <Grid item xs={6}>
+                    <Paper className={classes.paper} component="form">
+                        <DnDFlow/>
+                    </Paper>
+                </Grid>
+            </Grid>
         </div>
     )
 }
 
 export default CreateStrategy
+
