0d80456abdf1b6f154fe3ba1786b8815fa6bb75e
PierreVerbe <pierre.verbe@free.fr>
2022-02-17 22:42:57 +0100

    Added feature to generate zip

--- ui/src/component/strategy/step/create/left/StrategySimulator.js
+++ ui/src/component/strategy/step/create/left/StrategySimulator.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { connect } from "react-redux"
 
+import * as JSZip from 'jszip'
+import { saveAs } from 'file-saver';
+
 import Button from '@material-ui/core/Button';
 import parserStrategy from '../../../../../util/parserStrategy';
 
@@ -8,6 +11,18 @@ const StrategySimulator = ({strategyCreator, metaActionArray}) => {
     const handleParser = () => {
         const contentParser = parserStrategy.parse(strategyCreator, metaActionArray)
         console.log(contentParser)
+
+        var zip = new JSZip();
+        zip.file("strategy.json", JSON.stringify(contentParser.strategy))
+        contentParser.meta.forEach(element => {
+            zip.file(element.MetaActionName + ".json", JSON.stringify(element));
+        });
+        
+        zip.generateAsync({type:"blob"})
+        .then(function(content) {
+        // see FileSaver.js
+        saveAs(content, "example.zip");
+});
     }
 
     return (
