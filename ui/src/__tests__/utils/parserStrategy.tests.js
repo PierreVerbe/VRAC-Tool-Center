405e04fd3ee20cc36484ca1da75d0aedaa6dc4fc
Pierre Verbe <pierre.verbe@free.fr>
2022-02-23 23:13:50 +0100

    Added parserStrategy.tests.js

---
+++ ui/src/__tests__/utils/parserStrategy.tests.js
@@ -0,0 +1,17 @@
+import parserStrategy from './../../util/parserStrategy'
+
+describe("Test parserStrategy methods", () => {
+
+    test("Test of the parse method", () => {
+        expect(1).toBe(1)
+    })
+
+    test("Test of the parseStrategyCreator method", () => {
+        expect(1).toBe(1)
+    })
+
+    test("Test of the parseMetaActionArray method", () => {
+        expect(1).toBe(1)
+    })
+    
+})
