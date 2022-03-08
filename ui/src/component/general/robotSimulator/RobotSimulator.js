1fddc27a8b8b7c9889e8bfe3af9281fbc93562b0
Pierre Verbe <pierre.verbe@free.fr>
2022-03-08 22:41:46 +0100

    Added mouse position on canvas

--- ui/src/component/general/robotSimulator/RobotSimulator.js
+++ ui/src/component/general/robotSimulator/RobotSimulator.js
@@ -1,14 +1,20 @@
 import React, { useRef, useEffect } from "react"
 import { connect } from "react-redux"
+import Typography from '@material-ui/core/Typography';
 
 import { TABLE_WIDTH, TABLE_HEIGHT, REDUCING_FACTOR, RobotField } from "./RobotField"
 import FieldImage from "./../../../resources/image/AgeOfBots/fondTable22.jpg"
 
 const RobotSimulator = ({ monitoring, activeStepMonitoring }) => {
+    const [canvasX, setCanvasX] = React.useState(null);
+    const [canvasY, setCanvasY] = React.useState(null);
+
     const fieldImage = new Image()
     fieldImage.src = FieldImage
 
     const canvasRef = useRef(null)
+
+    
 
     useEffect(() => {
         const canvas = canvasRef.current
@@ -18,10 +24,17 @@ const RobotSimulator = ({ monitoring, activeStepMonitoring }) => {
             const robotField = new RobotField(context, fieldImage)
             robotField.render(monitoring, activeStepMonitoring)
         }
-    })
+
+        canvasRef.current.addEventListener("mousemove", function(e) { 
+            var cRect = canvasRef.current.getBoundingClientRect()
+            setCanvasX(Math.round((e.clientX - cRect.left) * REDUCING_FACTOR) )
+            setCanvasY(Math.round((e.clientY - cRect.top) * REDUCING_FACTOR) )   
+        })
+    }, [])
 
     return (
         <div>
+            <Typography variant="body1">Position on Canvas: x={canvasX} y={canvasY}</Typography>
             <canvas ref={canvasRef} width={TABLE_WIDTH / REDUCING_FACTOR} height={TABLE_HEIGHT / REDUCING_FACTOR}>
                 Désolé, votre navigateur ne prend pas en charge &ltcanvas&gt.
             </canvas>
