327f03b98a1541ca69e4d32e21d06de61490faaa
Pierre Verbe <pierre.verbe@free.fr>
2022-04-20 21:39:14 +0200

    Added Bezier Action in RobotAction Factory

--- ui/src/component/general/robotSimulator/RobotActionFactory.js
+++ ui/src/component/general/robotSimulator/RobotActionFactory.js
@@ -19,7 +19,73 @@ export const RobotActionFactory = (robot, action) => {
 
     switch (actionType) {
         case "Bezier":
+            console.log("Bezier")
+            console.log(robot)
+            console.log()
+
+            //const thetaXYT = - action.theta
+            const xBezier = action.x/REDUCING_FACTOR
+            const yBezier = action.y/REDUCING_FACTOR
+
+            const radianBezier = degreeToRadian(robot.angle + 90)
+            const xBezierRender = (action.radius / REDUCING_FACTOR) * Math.cos(radianBezier) + robot.x
+            const yBezierRender = (action.radius / REDUCING_FACTOR) * Math.sin(radianBezier) + robot.y
             
+            /*angle point controle et arrivee */
+            const angleBezier = calculateAngleDegrees(xBezier - xBezierRender ,  yBezier- yBezierRender ) + 90
+            
+            console.log("angleBezier")
+            console.log(angleBezier)
+
+            //outside 1
+            const radianBezier2 = degreeToRadian(robot.angle )
+            const radianBezier22 = degreeToRadian(angleBezier)
+
+            const xBezierRenderInitial2 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.cos(radianBezier2) + robot.x
+            const yBezierRenderInitial2 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.sin(radianBezier2) + robot.y
+
+            const xBezierRenderArrival2 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.cos(radianBezier22) + xBezier
+            const yBezierRenderArrival2 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.sin(radianBezier22) + yBezier
+
+            const xBezierRenderControlPoint2 = (action.radius / REDUCING_FACTOR) * Math.cos(radianBezier) + xBezierRenderInitial2
+            const yBezierRenderControlPoint2 = (action.radius / REDUCING_FACTOR) * Math.sin(radianBezier) + yBezierRenderInitial2
+
+            // outside line 2
+            const radianBezier1 = degreeToRadian(robot.angle + 90 + 90)
+            const radianBezier11 = degreeToRadian(angleBezier + 90 + 90)
+
+            const xBezierRenderInitial1 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.cos(radianBezier1) + robot.x
+            const yBezierRenderInitial1 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.sin(radianBezier1) + robot.y
+
+            const xBezierRenderArrival1 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.cos(radianBezier11) + xBezier
+            const yBezierRenderArrival1 = ((ROBOT_WIDTH /2) / REDUCING_FACTOR) * Math.sin(radianBezier11) + yBezier
+
+            const xBezierRenderControlPoint1 = (action.radius / REDUCING_FACTOR) * Math.cos(radianBezier) + xBezierRenderInitial1
+            const yBezierRenderControlPoint1 = (action.radius / REDUCING_FACTOR) * Math.sin(radianBezier) + yBezierRenderInitial1
+            //const xBezierRender11 = (action.radius / REDUCING_FACTOR) * Math.cos(radianBezier) + xBezierRenderInitial1
+            //const yBezierRender11 = (action.radius / REDUCING_FACTOR) * Math.sin(radianBezier) + yBezierRenderInitial1
+            
+
+            
+
+
+            const renderBezier = g => {
+                g.clear()
+                g.lineStyle(3, insideLine, 1)
+               
+                g.moveTo(robot.x, robot.y)
+                g.bezierCurveTo(xBezierRender, yBezierRender, xBezierRender, yBezierRender, yBezier, xBezier) // pts de controles les mêmes == quadratique
+                
+                g.lineStyle(3, outsideLine, 1)
+                g.moveTo(xBezierRenderInitial1, yBezierRenderInitial1)
+                g.bezierCurveTo(xBezierRenderControlPoint1, yBezierRenderControlPoint1, xBezierRenderControlPoint1, yBezierRenderControlPoint1, yBezierRenderArrival1, xBezierRenderArrival1) // pts de controles les mêmes == quadratique
+                
+                g.lineStyle(3, outsideLine, 1)
+                g.moveTo(xBezierRenderInitial2, yBezierRenderInitial2)
+                g.bezierCurveTo(xBezierRenderControlPoint2, yBezierRenderControlPoint2, xBezierRenderControlPoint2, yBezierRenderControlPoint2, yBezierRenderArrival2, xBezierRenderArrival2) // pts de controles les mêmes == quadratique
+                
+                g.endFill()
+              }
             
             return {x:xBezier, y:yBezier, t:angleBezier, render: renderBezier}
         
